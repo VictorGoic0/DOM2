a0c424aadd5cdb82629f5aeffcbf5725e87538e3
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-01-29 17:20:53 -0500

    variables for anchors and h1 added. event listeners for anchors, h1, and window added

--- js/index.js
+++ js/index.js
@@ -5,5 +5,11 @@ button.forEach(element => element.addEventListener('click', event => console.log
 const images = Array.from(document.getElementsByTagName('img'));
 images.forEach(element => element.addEventListener('mouseover', event => console.log('This event listener is also working.')));
 
-const body = document.querySelector('body');
-body.addEventListener('scroll', event => console.log('You are scrolling'));
+const anchors = Array.from(document.getElementsByTagName('a'));
+anchors.forEach(element => element.addEventListener('dblclick', event => element.style.color = 'red'));
+
+window.addEventListener('scroll', event => console.log('You are scrolling'));
+window.addEventListener('resize', event => console.log('This window has been resized.'));
+
+const h1 = document.querySelector('h1');
+h1.addEventListener('focus', event => h1.style.fontSize = '10rem');
