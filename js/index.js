8caf0d6dbce5f86bbbdde00a5efb5957a30a1946
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-01-29 16:56:51 -0500

    variables for buttons, images, and body tag added. Event listeners for each of these variables added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+const button = Array.from(document.getElementsByClassName('btn'));
+button.forEach(element => element.addEventListener('click', event => console.log('This event listener is working.')));
+
+const images = Array.from(document.getElementsByTagName('img'));
+images.forEach(element => element.addEventListener('mouseover', event => console.log('This event listener is also working.')));
+
+const body = document.querySelector('body');
+body.addEventListener('scroll', event => console.log('You are scrolling'));
