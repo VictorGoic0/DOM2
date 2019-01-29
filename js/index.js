9e9a7df9be0a627806d171ad27768894d1a03ef3
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-01-29 18:56:30 -0500

    variables and event listeners for h2 and h4 added

--- js/index.js
+++ js/index.js
@@ -6,10 +6,16 @@ const images = Array.from(document.getElementsByTagName('img'));
 images.forEach(element => element.addEventListener('mouseover', event => console.log('This event listener is also working.')));
 
 const anchors = Array.from(document.getElementsByTagName('a'));
-anchors.forEach(element => element.addEventListener('dblclick', event => element.style.color = 'red'));
+anchors.forEach(element => element.addEventListener('dblclick', event => element.style.color = '#c31d1d'));
 
 window.addEventListener('scroll', event => console.log('You are scrolling'));
 window.addEventListener('resize', event => console.log('This window has been resized.'));
 
 const h1 = document.querySelector('h1');
-h1.addEventListener('focus', event => h1.style.fontSize = '10rem');
+window.addEventListener('keydown', event => h1.style.fontSize = '10rem');
+
+const h2 = Array.from(document.getElementsByTagName('h2'));
+h2.forEach(element => element.addEventListener('drag', event => element.style.color = '#0064ff'))
+
+const h4 = Array.from(document.getElementsByTagName('h4'));
+h4.forEach(element => element.addEventListener('mousemove', event => element.style.color = '#ff00e9'))
